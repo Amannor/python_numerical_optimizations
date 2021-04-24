ea6400ae53527053647b962785c49ec84ef82922
Amannor <alon.mannor@gmail.com>
2021-04-24 20:34:15 +0300

    Update utils.py

--- ex1/programming_part/code/src/utils.py
+++ ex1/programming_part/code/src/utils.py
@@ -2,31 +2,35 @@ import matplotlib
 import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+import os
+EPSILON = 0.005
+PARENT_DIR = os.path.dirname(os.getcwd())
 
 def return_function_val_only(f, arg):
     return f(arg)[0]
 
-def plot_contours_and_paths(x, y):
-    print(f'len(x) {len(x)} len(y) {len(y)}')
+def plot_contours_and_paths(f, x):
+    c0 = [coordinate[0] for coordinate in x]
+    c1 = [coordinate[0] for coordinate in x]
+
 
     # Following code based on https://matplotlib.org/stable/gallery/images_contours_and_fields/contour_demo.html
-
-    delta = 0.025
-    # x = np.arange(-3.0, 3.0, delta)
-    # y = np.arange(-2.0, 2.0, delta)
-    # X, Y = np.meshgrid(x, y)
-    # Z1 = np.exp(-X ** 2 - Y ** 2)
-    # Z2 = np.exp(-(X - 1) ** 2 - (Y - 1) ** 2)
-    # Z = (Z1 - Z2) * 2
-
-    # X1, X2 = np.meshgrid([coordinate[0] for coordinate in x], [coordinate[1] for coordinate in x])
-    X1, X2 = [coordinate[0] for coordinate in x], [coordinate[1] for coordinate in x]
+    X1, X2 = np.meshgrid(c0, c1)
     fig, ax = plt.subplots()
-    # CS = ax.contour(X, Y, Z)
-    CS = ax.contour(X1, X2, y)
+    plt.xlim(min(-1.7,min(c0)-EPSILON),max(1.7,max(c0)+EPSILON))
+    plt.ylim(min(-1.7,min(c1)-EPSILON),max(1.7,max(c1)+EPSILON))
+    Z = np.empty(shape=(X1.shape[0],X1.shape[1]))
+    for i in range(X1.shape[0]):
+        for j in range(X1.shape[1]):
+            Z[i][j] = f(np.array([X1[i][j], X2[i][j]]))[0]
+    CS = ax.contour(X1, X2, Z)
     ax.clabel(CS, inline=True, fontsize=10)
-    ax.set_title('Simplest default with labels')
-    plt.show()
+    ax.set_title(f'{f.__name__}')
+    ax.scatter(c0, c1)
+
+
+    path = os.path.join(PARENT_DIR, 'output', f'{f.__name__}.png')
+    plt.savefig(path)
 
 def report_iteration(iter_count, cur_location, cur_obj_val, cur_step_len, cur_obj_f_val_change):
     print(f'Iteration number: {iter_count} current location: {cur_location} current obj val: {cur_obj_val} current step length: {cur_step_len} current change in objective function value: {cur_obj_f_val_change}')
