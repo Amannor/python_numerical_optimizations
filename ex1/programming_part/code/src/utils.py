7d1a06670891ca3a187bfe764a6030110636c86f
Amannor <alon.mannor@gmail.com>
2021-04-25 20:02:30 +0300

    Improved the contour code

--- ex1/programming_part/code/src/utils.py
+++ ex1/programming_part/code/src/utils.py
@@ -3,23 +3,26 @@ import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import os
-EPSILON = 0.005
+
+MIN_AXIS_REACH = 1.5
 PARENT_DIR = os.path.dirname(os.getcwd())
 
 def plot_contours_and_paths(f, x):
     c0 = np.array([coordinate[0] for coordinate in x])
     max_abs_val0 = max(abs(min(c0)), max(c0))
+    max_abs_val0 = max(max_abs_val0,MIN_AXIS_REACH)
     linspace0 = np.linspace(-max_abs_val0, max_abs_val0)
     c1 = np.array([coordinate[1] for coordinate in x])
     max_abs_val1 = max(abs(min(c1)), max(c1))
+    max_abs_val1 = max(max_abs_val1, MIN_AXIS_REACH)
     linspace1 = np.linspace(-max_abs_val1, max_abs_val1)
 
 
     # Following code based on https://matplotlib.org/stable/gallery/images_contours_and_fields/contour_demo.html
     X1, X2 = np.meshgrid(linspace0, linspace1)
     fig, ax = plt.subplots()
-    plt.xlim(-max_abs_val0, max_abs_val0)
-    plt.ylim(-max_abs_val1, max_abs_val1)
+    plt.xlim(-max_abs_val0,max_abs_val0)
+    plt.ylim(-max_abs_val1,max_abs_val1)
     Z = np.empty(shape=(X1.shape[0],X1.shape[1]))
     for i in range(X1.shape[0]):
         for j in range(X1.shape[1]):
